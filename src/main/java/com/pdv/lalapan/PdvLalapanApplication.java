1919df1ac97e2d2eecc8ee2d2b5a830b66fbd24a
comcod <netosilvaneto6@gmail.com>
2025-12-02 10:19:54 -0300

    commit inicial

---
+++ src/main/java/com/pdv/lalapan/PdvLalapanApplication.java
@@ -0,0 +1,13 @@
+package com.pdv.lalapan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvLalapanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvLalapanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdv/lalapan/PdvLalapanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdv.lalapan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvLalapanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
