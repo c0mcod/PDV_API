b33822ee93605881a5c0d3b9f2293eedeaed222a
c0mcod <netosilvaneto6@gmail.com>
2026-03-08 23:05:33 -0300

    Correção na inicialização para o spring serializar Page num formato fixo e estável

--- src/main/java/com/pdv/lalapan/PdvLalapanApplication.java
+++ src/main/java/com/pdv/lalapan/PdvLalapanApplication.java
@@ -3,7 +3,11 @@ package com.pdv.lalapan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class PdvLalapanApplication {
 
 	public static void main(String[] args) {
